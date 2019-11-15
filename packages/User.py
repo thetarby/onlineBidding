bc0c0e520659c5443add1a2d46778a47281e65c4
azad <azad.baykara96@gmail.com>
2019-11-15 13:45:02 +0300

    User verify method is implemented

--- packages/User.py
+++ packages/User.py
@@ -1,3 +1,5 @@
+import re
+
 class User:
     def __init__(self,email,name_surname,password,watcher):
         self.email=email
@@ -8,7 +10,14 @@ class User:
         self.owned_items={}
 
     def verify(self,email,verification_number):
-        print(1)
+        email_form = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$'
+        if re.match(email_form, email) != None:
+            if verification_number == self.password:
+                print('User is verified.')
+            else:
+                raise ValueError('Wrong password')
+        else:
+            raise ValueError('Enter a valid email address')          
 
     def change_password(self,new_password,old_password=None):
         if(old_password is not None):
