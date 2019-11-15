b97b9f4acd26638e7e5fba948db23aa7a2525956
azad <azad.baykara96@gmail.com>
2019-11-15 20:10:18 +0300

    methods were updated

--- packages/SellItem.py
+++ packages/SellItem.py
@@ -16,19 +16,35 @@ class SellItem:
 
     def start_auction(self, topbid=None):
         self.watcher.notify(self.item_type)
+        self.state = 'onhold'
 
     
     def bid(self, user, amount):
+        if amount <= self.amount:
+            raise ValueError('Amount should be more than the last bid')
+        if self.state == 'sold':
+            raise ValueError('Auction is not active')
+        if user.get_balance() - user.get_reserved() < amount:
+            raise ValueError('Cannot bid that much amount')
+        
+
         self.highest_payer = user
         self.amount = amount
 
         
     def sell(self):
         self.highest_payer.buy(self.item_type,self.amount)
+        self.state = 'sold'
 
     
     def view(self):
-        print(1)
+        print('owner: ', self.owner)
+        print('title: ', self.title)
+        print('item_type: ', self.item_type)
+        print('description: ', self.decsription)
+        print('bidtype: ', self.bidtype)
+        print('starting: ', self.starting)
+        print('minbid: ', self.minbid)
 
 
     def watch(self, user,method):
--- packages/User.py
+++ packages/User.py
@@ -9,6 +9,8 @@ class User:
         self.watcher=watcher
         self.owned_items={}
         self.enable = False
+        
+
 
     def verify(self,email,verification_number):
         email_form = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$'
@@ -20,6 +22,7 @@ class User:
         else:
             raise ValueError('Enter a valid email address')          
 
+
     def change_password(self,new_password,old_password=None):
         if(old_password is not None):
             if(old_password==self.password):
@@ -28,7 +31,7 @@ class User:
                 print('error')
         else:
             self.password=new_password
-        # kardo adam eski sifreyi vermezse direkt sifresini degistiriyoz boyle sacma degil mi?
+
 
     def list_items(self, item_type=None, state='all'):
         items = []
@@ -48,22 +51,32 @@ class User:
         
         return items
 
+
     def watch(self,watch_method,item_type=None):
         self.watcher.register(self, watch_method,item_type)
+
 
     def add_balance(self,amount):
         self.balance=self.balance+amount
 
     
     def buy(self,item,item_type,price):
-        self.balance-=price
+        if price > self.balance:
+            raise ValueError('Not enough money')
+
+        self.balance -= price
         if(item_type not in self.owned_items):
             self.owned_items[item_type]=[]
         self.owned_items[item_type].append(item)
     
 
-
     def report(self):
         print(1)
 
+
+    def get_balance(self):
+        return self.balance
+
+    def get_reserved(self):
+        return self.reserved
     
